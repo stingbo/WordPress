a3a407794beb81a3599499713698e6cfa3e4ce33
wanlianbo <sting_bo@163.com>
2016-11-17 17:38:25 +0800

    修改页脚链接

--- wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
+++ wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
@@ -10,5 +10,5 @@
 
 ?>
 <div class="site-info">
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?></a>
+	<a href="<?php echo esc_url( __( 'http://blog.blianb.com/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'bobo' ); ?></a>
 </div><!-- .site-info -->
