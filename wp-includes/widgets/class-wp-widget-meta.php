9d6805423a724a244f07a1972d044df023ab6d67
wanlianbo <lianbo.wan@gmail.com>
2017-09-23 00:48:53 +0800

    修改首页土豆海淘地址链接

--- wp-includes/widgets/class-wp-widget-meta.php
+++ wp-includes/widgets/class-wp-widget-meta.php
@@ -73,7 +73,7 @@ class WP_Widget_Meta extends WP_Widget {
 			) );
             */
             echo apply_filters( 'widget_meta_poweredby', sprintf( '<li><a href="%s" title="%s">%s</a></li>',
-                esc_url( __( 'http://www.tudouhaitao.com/' ) ),
+                esc_url( __( 'http://tudouhaitao.blianb.com/' ) ),
                 esc_attr__( 'Powered by BoBo, 个人项目DEMO演示.' ),
                 _x( '土豆海淘', 'meta widget link text' )
             ) );
